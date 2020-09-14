45902da4166c798ec94724f9accdc709cb62f78d
Juan Camilo Rojas <dkcam80@gmail.com>
2020-09-13 20:04:51 -0500

    hotfix cambio de datos

--- src/main/java/edu/eci/bp/App.java
+++ src/main/java/edu/eci/bp/App.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 public class App 
 {
     public static void main( String[] args ) {
-        double[] data= {1.0,25.0,6.0,3.5,0.1,2.5,3.5,25.0};
+        double[] data= {1.0,1.1,1.2,1.3,1.4,1.5,1.5,1.5,1.6,1.7,1.8,1.9,2.0};
         System.out.println( "La media de los datos es :"+StatisticsModule.mean(data) );
         System.out.println( "La desviacion estándar de los datos es :"+StatisticsModule.standardDeviation(data) );
         System.out.println( "La moda de los datos es :"+StatisticsModule.mode(data) );
